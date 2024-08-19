3f4db2b1711bcbb36066db4a7e71aa36117848d2
jcorreaw <juninhocorrea2014@gmail.com>
2024-08-19 18:30:03 -0300

    Projeto Calculadora Concluido

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ola mundo!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Escolha uma das opções: \n 01: Soma \n 02: Subtração \n 03: Multiplicação  \n 04: Divisão: \n Opção escolhida: ");
+        int operacao = scanner.nextInt();
+
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        double resultado = 0;
+
+        switch (operacao) {
+            case 1:
+                resultado = num1 + num2;
+                System.out.println("Resultado da soma: " + resultado);
+                break;
+            case 2:
+                resultado = num1 - num2;
+                System.out.println("Resultado da subtração: " + resultado);
+                break;
+            case 3:
+                resultado = num1 * num2;
+                System.out.println("Resultado da multiplicação: " + resultado);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                    System.out.println("Resultado da divisão: " + resultado);
+                } else {
+                    System.out.println("Erro: Divisão por zero não é permitida.");
+                }
+                break;
+            default:
+                System.out.println("Operação inválida!");
+                break;
+        }
+
+        scanner.close();
     }
 }
