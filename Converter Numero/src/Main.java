e3bbd2cc808e9a48e7353f94cf6554fb6a14709e
jcorreaw <juninhocorrea2014@gmail.com>
2024-08-19 18:44:14 -0300

    Converter Numeros Concluido

--- Converter Numero/src/Main.java
+++ Converter Numero/src/Main.java
@@ -1,7 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escolha a conversão desejada:");
+        System.out.println("1. Int para Double");
+        System.out.println("2. Double para Int");
+
+        System.out.print("Digite o número da opção (1/2): ");
+        int opcao = scanner.nextInt();
+
+        if (opcao == 1) {
+            System.out.print("Digite um número inteiro: ");
+            int numeroInt = scanner.nextInt();
+            System.out.println("Número em double: " + (double) numeroInt);
+        } else if (opcao == 2) {
+            System.out.print("Digite um número double: ");
+            double numeroDouble = scanner.nextDouble();
+            int numeroInt = (int) numeroDouble;
+            System.out.println("Número em int: " + numeroInt);
+        } else {
+            System.out.println("Opção inválida!");
+        }
+
+        scanner.close();
     }
 }
