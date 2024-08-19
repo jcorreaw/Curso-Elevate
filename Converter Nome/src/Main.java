170b9fbadbb422f4b0a0fa73db2adc68d5bcf47e
jcorreaw <juninhocorrea2014@gmail.com>
2024-08-19 18:40:34 -0300

    Converter String concluido

--- Converter Nome/src/Main.java
+++ Converter Nome/src/Main.java
@@ -1,7 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a primeira string: ");
+        String string1 = scanner.nextLine();
+
+        System.out.print("Digite a segunda string: ");
+        String string2 = scanner.nextLine();
+
+        String concatenado = string1 + string2;
+
+        String caixaAlta = concatenado.toUpperCase();
+        String caixaBaixa = concatenado.toLowerCase();
+
+        int quantidadeLetras = concatenado.length();
+
+        System.out.println("String concatenada: " + concatenado);
+        System.out.println("Em caixa alta: " + caixaAlta);
+        System.out.println("Em caixa baixa: " + caixaBaixa);
+        System.out.println("Quantidade de letras: " + quantidadeLetras);
+
+        scanner.close();
     }
 }
