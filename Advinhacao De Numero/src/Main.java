75b3f7d7d3d9feeb89214e2f88a2adbefb638cf1
jcorreaw <juninhocorrea2014@gmail.com>
2024-08-20 20:23:51 -0300

    Advinhacao De Numero Concluido

--- Advinhacao De Numero/src/Main.java
+++ Advinhacao De Numero/src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ola mundo!");
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroAleatorio = (int) (Math.random() * 100) + 1;
+        int palpite = 0;
+        int tentativas = 0;
+
+        System.out.println("Adivinhe o número entre 1 e 100:");
+
+        while (palpite != numeroAleatorio) {
+            System.out.print("Seu palpite: ");
+            palpite = scanner.nextInt();
+            tentativas++;
+
+            if (palpite < numeroAleatorio) {
+                System.out.println("O número é maior.");
+            } else if (palpite > numeroAleatorio) {
+                System.out.println("O número é menor.");
+            } else {
+                System.out.println("Parabéns! Você acertou em " + tentativas + " tentativas.");
+            }
+        }
+
+        scanner.close();
     }
 }
