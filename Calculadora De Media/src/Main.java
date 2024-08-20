9de0222925f018266cb8d12291761fd025df4e54
jcorreaw <juninhocorrea2014@gmail.com>
2024-08-20 20:11:30 -0300

    Calculadora de Media Concluido

--- Calculadora De Media/src/Main.java
+++ Calculadora De Media/src/Main.java
@@ -1,7 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Ola Mundo!");
+        Scanner sc = new Scanner(System.in);
+
+        double[] num = new double[5];
+        double soma = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.print("Digite o " + (i + 1) + "º numero: ");
+            num[i] = sc.nextDouble();
+            soma += num[i];
+        }
+
+        double media = soma / num.length;
+        System.out.println("A media total é: " + media);
+
+        sc.close();
     }
 }
